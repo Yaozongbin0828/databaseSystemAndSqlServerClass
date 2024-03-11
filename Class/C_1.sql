5183aa58bcc2475d698796ef7dec3f63367ef5d5
yaozongbin <78633585+yaozongbin@users.noreply.github.com>
2024-03-11 23:31:44 +0800

    Update C_1.sql

--- Class/C_1.sql
+++ Class/C_1.sql
@@ -1,14 +1,23 @@
+-- 数据库student
 CREATE DATABASE student
-CREATE TABLE Student(
-	Sno VARCHAR(9) PRIMARY KEY,
-	Sname VARCHAR(10)NOT NULL,
-	Ssex VARCHAR(2),
-	Sage TINYINT,
-	Sdept VARCHAR(20) DEFAULT '计算机系',
-	Sid VARCHAR(18),
-	Sdate DATE
-);
 
+-- Student表
+CREATE TABLE Student(
+    Sno CHAR(9) PRIMARY KEY,
+    Sname VARCHAR(10) NOT NULL,
+    Ssex CHAR(2),
+    CONSTRAINT CK_Ssex CHECK (Ssex IN ('男','女')),
+    Sage TINYINT, 
+    CONSTRAINT CK_Sage CHECK (Sage >= 15 AND Sage <= 45),
+    Sdept VARCHAR(20) DEFAULT '计算机系',
+    Sid CHAR(18) UNIQUE
+    -- Sdate DATETIME DEFAULT CURRENT_TIMESTAMP,
+);
+-- Sdate
+ALTER TABLE Student
+ADD Sdate DATETIME DEFAULT CURRENT_TIMESTAMP;
+
+-- 插入值
 INSERT INTO Student VALUES
 (1,'aaa','男',20,'计算机系',4891,'2023-09-03'),
 (2,'bbb','男',21,'计算机系',4892,'2023-09-03'),
@@ -19,33 +28,39 @@ INSERT INTO Student VALUES
 (7,'ggg','男',24,'计算机系',4897,'2023-09-03'),
 (8,'hhh','女',24,'计算机系',4898,'2023-09-03')
 
+-- Course表
 CREATE TABLE Course(
-	Cno VARCHAR(10)  PRIMARY KEY,
-	Cname CHAR(20) NOT NULL,
-	Credit FLOAT,
-	Semester FLOAT,
+	Cno CHAR(10) PRIMARY KEY,
+	Cname VARCHAR(20) NOT NULL,
+	Credit DECIMAL(3, 1) CHECK (Credit > 0),
+	Semester TINYINT
 )
 
-CREATE TABLE SC(
-	Sno VARCHAR(9),
-	Cno VARCHAR(10),
-	Grade TINYINT,
-	FOREIGN KEY(Sno) REFERENCES Student(Sno),
-	FOREIGN KEY (Cno) REFERENCES Course(Cno),
-)
+-- SC表
+CREATE TABLE SC (
+    Sno CHAR(9),
+    Cno CHAR(10),
+    Grade TINYINT,
+    FOREIGN KEY (Sno) REFERENCES Student(Sno),
+    FOREIGN KEY (Cno) REFERENCES Course(Cno),
+    CHECK (Grade >= 0 AND Grade <= 100)
+);
 
+-- Teacher表
 CREATE TABLE Teacher(
 	Tno CHAR(8) NOT NULL,
 	Tname CHAR(10) NOT NULL,
-	Salary FLOAT
+	-- 定点小数，小数点前4位，小数点后2位
+	Salary DECIMAL(6, 2)
 )
 
-SELECT * FROM Student;
+-- 查表
+SELECT * FROM Student
 SELECT * FROM Course
 SELECT * FROM SC
 SELECT * FROM Teacher
 
--- SQL_USE
+-- 操作
 -- 1.Teacher表添加职称列，列名Title,类型为nchar(4)
 ALTER TABLE Teacher ADD Title nchar(4);
 -- 2.Teacher表中的Title列添加取值范围约束，取值约束范围{教授，副教授，讲师}
